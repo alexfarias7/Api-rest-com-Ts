1cd50021f4501caaba5a1c54450d6233c4501950
alexfarias7 <alexfarias44@gmail.com>
2022-06-15 04:14:12 -0300

    create routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import  Express  from "express";
-import  config  from "config";
+import Express from "express";
+import config from "config";
+import router from "./router";
 
-const app = Express()
+const app = Express();
 //* json middleware
-app.use(Express.json())
+app.use(Express.json());
 //* app port
-const port = config.get<number>('port')
+const port = config.get<number>("port");
+//* app routes
+app.use("/api/", router);
 
-app.listen(port,async()=>{
-    console.log(`app rodando na porta:${port}`)
-})
+app.listen(port, async () => {
+  console.log(`app rodando na porta:${port}`);
+});
