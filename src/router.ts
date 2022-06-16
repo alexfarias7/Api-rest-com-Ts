5df965c076b9fb38cf28d661d7fdd931291310ff
alexfarias7 <alexfarias44@gmail.com>
2022-06-16 02:52:44 -0300

    add controller

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieControllers";
 
 const router = Router()
 
 export default router.get('/test', (req:Request, res:Response)=>{
     res.status(200).send('api esta funcionando normal')
-})
+}).post('/movie', createMovie)
